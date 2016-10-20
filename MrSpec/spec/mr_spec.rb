469d78e622565ddf45bf74678f49d4cec28d964a
Fede <federico.lochbaum@gmail.com>
2016-10-19 22:49:56 -0300

    mrSpec

---
+++ MrSpec/spec/mr_spec.rb
@@ -0,0 +1,10 @@
+class MrSpec
+
+  def getMrSpecTests klass
+    klass.instance_methods.select {|method| self.isMrSpecTest method}
+  end
+
+  def isMrSpecTest method
+    method.to_s.end_with? 'MrSpec'
+  end
+end
